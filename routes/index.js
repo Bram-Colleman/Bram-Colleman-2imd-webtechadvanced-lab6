bcc06480f9b3a26004965bc42d04ae1e3130f7a3
Bram-Colleman <r0820751@student.thomasmore.be>
2022-03-27 11:14:14 +0200

    refactor: change normal function to arrow function

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/updatestats', function(req, res, next) {
+router.get('/updatestats', (req, res, next) => {
   res.render('updatestats');
 });
 
